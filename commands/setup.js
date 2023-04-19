ab3accebbc36fcdb5d8a721e1f2d2b0702f7edbb
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2023-04-18 22:37:09 -0700

    testing fo command

--- commands/setup.js
+++ commands/setup.js
@@ -111,23 +111,23 @@ module.exports = {
         if (teamOption !== "3") {
             const roles = await interaction.guild.roles.fetch()
             let clonedArray = structuredClone(teamJson)
-            let foExists = false
-            let gmExists = false
-            let hcExists = false
+            let foExists = true
+            let gmExists = true
+            let hcExists = true
             for (const role of roles.values()) {
                 // first, check if the role is already in the DB
                 const roleExists = await db.get('SELECT * FROM Roles WHERE roleid = ? AND guild = ?', role.id, guild)
                 if (!roleExists) {
                     if (role.name.toLowerCase() === "franchise owner") {
-                        foExists = true
+                        foExists = false
                         await db.run('INSERT INTO Roles (code, roleid, guild) VALUES (?, ?, ?)', ["FO", role.id, guild]);
                     }
                     if (role.name.toLowerCase() === "general manager") {
-                        gmExists = true
+                        gmExists = false
                         await db.run('INSERT INTO Roles (code, roleid, guild) VALUES (?, ?, ?)', ["GM", role.id, guild]);
                     }
                     if (role.name.toLowerCase() === "head coach") {
-                        hcExists = true
+                        hcExists = false
                         await db.run('INSERT INTO Roles (code, roleid, guild) VALUES (?, ?, ?)', ["HC", role.id, guild]);
                     }
                     for (let i = 0; i < teamJson.length; i++) {
