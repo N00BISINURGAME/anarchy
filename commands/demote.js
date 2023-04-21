b8ce203ccc70bcaee1acf100c8ea6f0c08cf2240
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2023-04-21 10:53:43 -0700

    pushing demote command up

--- commands/demote.js
+++ commands/demote.js
@@ -102,7 +102,7 @@ module.exports = {
             .setTitle('Player demoted!')
             .setThumbnail(logoStr)
             .setFooter({ text: `${interaction.user.tag}`, iconURL: `${interaction.user.avatarURL()}` })
-            .setDescription(`The ${teamRole}'s Franchise Owner has demoted ${userChoice} from ${specialRoleObj}!\n>>> **Franchise Owner:** ${interaction.member}`)
+            .setDescription(`The ${teamRole}'s Franchise Owner has demoted ${userChoice} (${userChoice.user.tag}) from ${specialRoleObj}!\n>>> **Franchise Owner:** ${interaction.member} (${interaction.user.tag})`)
             .setColor(teamRole.color)
 
         const channelId = await db.get('SELECT channelid FROM Channels WHERE purpose = "transactions" AND guild = ?', guild)
