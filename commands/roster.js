ff6a4e17d9189f3bec05468ec20d38c5047c1509
Donovan Kong <kongdonovan@hotmail.com>
2023-04-22 14:53:40 -0700

    did offer

--- commands/roster.js
+++ commands/roster.js
@@ -38,10 +38,10 @@ module.exports = {
                 for (const role of member.roles.cache.keys()) {
                     const roleExists = await db.get('SELECT code FROM Roles WHERE roleid = ? AND guild = ?', [role, guild])
                     if (roleExists) {
-                        if (roleExists.code === "FO") fo = `${member}\n${member.user.tag}`;
-                        else if (roleExists.code === "GM") gm = `${member}\n${member.user.tag}`;
-                        else if (roleExists.code === "HC") hc = `${member}\n${member.user.tag}`;
-                        else players += `${member}\n${member.user.tag}\n`;
+                        if (roleExists.code === "FO") fo = `${member} (${member.user.tag})`;
+                        else if (roleExists.code === "GM") gm = `${member} (${member.user.tag})`;
+                        else if (roleExists.code === "HC") hc = `${member} (${member.user.tag})`;
+                        else players += `${member} (${member.user.tag})\n`;
                         break
                     }
                 }
