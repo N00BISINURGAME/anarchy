c96d8bcec917cfe7bf441cbcd6aea4932d833c21
Donovan Kong <kongdonovan@hotmail.com>
2023-04-23 13:48:32 -0700

    added bold command

--- commands/bold.js
+++ commands/bold.js
@@ -1,14 +1,15 @@
 const { SlashCommandBuilder, SlashCommandChannelOption, SlashCommandStringOption, ChannelType } = require('discord.js');
-const { serif } = require('weird-fonts')
+const { serif, script, sansSerif } = require('weird-fonts')
 
 const textOption = new SlashCommandStringOption().setName("text").setDescription("The text to boldify").setRequired(true)
 
 const typeChoices = new SlashCommandStringOption().setName("type").setDescription("The typeface you want to use").setRequired(true)
                           .addChoices(
-                            { name:"𝗦𝗮𝗻𝘀 𝗦𝗲𝗿𝗶𝗳 𝗕𝗼𝗹𝗱", value: "serif bold" },
-                            { name:"𝙎𝙖𝙣𝙨 𝙎𝙚𝙧𝙞𝙛 𝘽𝙤𝙡𝙙", value: "serif bold-italic" },
-                            { name:"𝓢𝓪𝓷𝓼 𝓢𝓮𝓻𝓲𝓯 𝓑𝓸𝓵𝓭", value: "script bold" },
-                            { name:"𝐁𝐨𝐥𝐝", value:"bold" }
+                            { name:"𝗦𝗮𝗻𝘀 𝗦𝗲𝗿𝗶𝗳 𝗕𝗼𝗹𝗱", value: "1" },
+                            { name:"𝙎𝙖𝙣𝙨 𝙎𝙚𝙧𝙞𝙛 𝘽𝙤𝙡𝙙", value: "2" },
+                            { name:"𝓢𝓪𝓷𝓼 𝓢𝓮𝓻𝓲𝓯 𝓑𝓸𝓵𝓭", value: "3" },
+                            { name:"𝐁𝐨𝐥𝐝", value:"4" },
+                            { name:"𝑩𝒐𝒍𝒅 𝑰𝒕𝒂𝒍𝒊𝒄", value:"5" }
                           )
 
 module.exports = {
@@ -20,7 +21,18 @@ module.exports = {
     async execute(interaction) {
       const text = interaction.options.getString('text')
       const type = interaction.options.getString('type')
-      const str = serif(`${text}`, { fontStyle: type })
+      let str
+      if (type === "1") {
+        str = sansSerif(`${text}`, { fontStyle:"bold" })
+      } else if (type === "2") {
+        str = sansSerif(`${text}`, { fontStyle:"bold-italic" })
+      } else if (type === "3") {
+        str = script(`${text}`, { fontStyle:"bold" })
+      } else if (type === "4") {
+        str = serif(`${text}`, { fontStyle:"bold" })
+      } else if (type === "5") {
+        str = serif(`${text}`, { fontStyle:"bold-italic" })
+      }
 
       return interaction.editReply({ content:`${str}`, ephemeral:true})
     }
