8606032de900ed95c80dd4b28adade46c00ff997
Donovan Kong <kongdonovan@hotmail.com>
2023-04-23 12:13:39 -0700

    asdfasdf

---
+++ commands/sign.js
@@ -0,0 +1,118 @@
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+const { SlashCommandBuilder, SlashCommandUserOption, SlashCommandStringOption, SlashCommandIntegerOption, SlashCommandNumberOption, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
+const { getDBConnection } = require('../getDBConnection');
+const { message } = require('noblox.js');
+
+
+let dmChannel;
+let dmMessage;
+let userMessage;
+const userOption = new SlashCommandUserOption();
+userOption.setRequired(true);
+userOption.setName('player');
+userOption.setDescription('The player to sign');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('sign')
+        .setDescription('Signs a player to your team.')
+        .addUserOption(userOption),
+    async execute(interaction) {
+        let pingedUser = interaction.options.getMember('player')
+        let userid = pingedUser.id;
+        const guild = interaction.guild.id
+        const db = await getDBConnection()
+        const maxPlayerCount = await db.get('SELECT maxplayers FROM Leagues WHERE guild = ?', guild)
+        const maxPlayers = maxPlayerCount.maxplayers
+        const offerEnabled = await db.get('SELECT offers FROM Leagues WHERE guild = ?', guild)
+        if (!offerEnabled.offers) return interaction.editReply({ content: "Offers are disabled for the postseason!", ephemeral: true})
+
+        // check if a transaction channel has been set
+        const transactionExists = await db.get('SELECT * FROM Channels WHERE purpose = "transactions" AND guild = ?', guild)
+        if (!transactionExists) {
+            return interaction.editReply({ content: "A transaction channel has not been set!", ephemeral: true})
+        }
+
+        if (user.id === interaction.user.id) {
+          return interaction.editReply({ content:"You are not allowed to offer yourself!", ephemeral:true })
+        }
+
+        // first, check and see if the user that sent the command is authorized to sign a player (as in, they are a FO or GM)
+        const userSent = interaction.user.id;
+        const allTeams = await db.all('SELECT roleid, code FROM Roles WHERE guild = ?', guild)
+        let authorized = false
+        let info
+        for (const team of allTeams) {
+            if (interaction.member.roles.cache.get(team.roleid)) {
+                if (team.code === "FO" || team.code === "GM") {
+                    authorized = true
+                }
+                if (!(team.code === "FO" || team.code === "GM" || team.code === "HC")) {
+                    info = team.code
+                }
+            }
+        }
+        if (!authorized) {
+            await db.close();
+            return interaction.editReply({ content:'You are not authorized to sign a player!', ephemeral:true});
+        }
+
+        // then, get the team logo
+        const logo = await db.get('SELECT logo FROM Teams WHERE code = ? AND guild = ?', [info, guild]);
+        let logoStr;
+        if (!logo) {
+            logoStr = interaction.client.user.avatarURL()
+        } else {
+            logoStr = logo.logo;
+        }
+
+        // then, check to see if the user is already signed
+        let userSigned = false
+        let teamSigned
+        for (const team of allTeams) {
+            if (userPing.roles.cache.get(team.roleid)) {
+                if (!(team.code === "FO" || team.code === "GM" || team.code === "HC")) {
+                    userSigned = true
+                    teamSigned = team.code
+                    break
+                }
+                
+            }
+        }
+        if (userSigned) {
+            // then, get the team that the player is signed on
+            const team = await db.get('SELECT roleid FROM roles WHERE code = ? AND guild = ?', [teamSigned, guild]);
+            const teamRole = await interaction.guild.roles.fetch(team.roleid);
+            await db.close();
+            return interaction.editReply({content:`This user has already been signed by the ${teamRole}!`, ephemeral:true});
+        }
+
+        const memberTeamRole = await db.get('SELECT roleid FROM Roles WHERE code = ? AND guild = ?', [info, guild])
+        const teamRole = await interaction.guild.roles.fetch(memberTeamRole.roleid)
+
+        if (teamRole.members.size + 1 > maxPlayers) return interaction.editReply({content:'Signing this player would lead to your team exceeding the maximum player count!', ephemeral:true});
+
+        let dmChannel = await userPing.createDM()
+
+        let dmMessage = new EmbedBuilder()
+                .setTitle("Player Signed!")
+                .setColor(teamRole.color)
+                .setThumbnail(logoStr)
+                .setDescription(`The ${teamRole.name} have signed you!
+                \n>>> **Coach:** ${interaction.user.tag}`)
+            if (interaction.user.avatarURL()) {
+                dmMessage.setFooter({ text: `${interaction.user.tag}`, iconURL: `${interaction.user.avatarURL()}` })
+            } else {
+                dmMessage.setFooter({ text: `${interaction.user.tag}` })
+            }
+        
+        await dmChannel.send({ embeds:[dmMessage] })
+
+        await userPing.roles.add(teamRole)
+
+        await db.close()
+
+        return interaction.editReply({content:`${userPing} has been successfully signed!`, ephemeral:true});
+    }
+}
