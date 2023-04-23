76ccf58bd7a220a88e3bf967dc493cb50abbad74
Donovan Kong <kongdonovan@hotmail.com>
2023-04-23 13:29:33 -0700

    added bold command

--- commands/bold.js
+++ commands/bold.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder, SlashCommandChannelOption, SlashCommandStringOption, ChannelType } = require('discord.js');
-const { transform, getFonts } = require('convert-unicode-fonts')
+const { serif } = require('weird-fonts')
 
 const textOption = new SlashCommandStringOption().setName("text").setDescription("The text to boldify").setRequired(true)
 
 const typeChoices = new SlashCommandStringOption().setName("type").setDescription("The typeface you want to use").setRequired(true)
                           .addChoices(
-                            { name:"𝗦𝗮𝗻𝘀 𝗦𝗲𝗿𝗶𝗳 𝗕𝗼𝗹𝗱", value: "sansSerifBold" },
-                            { name:"𝙎𝙖𝙣𝙨 𝙎𝙚𝙧𝙞𝙛 𝘽𝙤𝙡𝙙", value: "sansSerifBoldItalic" },
-                            { name:"𝓢𝓪𝓷𝓼 𝓢𝓮𝓻𝓲𝓯 𝓑𝓸𝓵𝓭", value: "scriptBold" },
+                            { name:"𝗦𝗮𝗻𝘀 𝗦𝗲𝗿𝗶𝗳 𝗕𝗼𝗹𝗱", value: "serif-bold" },
+                            { name:"𝙎𝙖𝙣𝙨 𝙎𝙚𝙧𝙞𝙛 𝘽𝙤𝙡𝙙", value: "serif-bold-italic" },
+                            { name:"𝓢𝓪𝓷𝓼 𝓢𝓮𝓻𝓲𝓯 𝓑𝓸𝓵𝓭", value: "script-bold" },
                             { name:"𝐁𝐨𝐥𝐝", value:"bold" }
                           )
 
@@ -20,8 +20,7 @@ module.exports = {
     async execute(interaction) {
       const text = interaction.options.getString('text')
       const type = interaction.options.getString('type')
-      const fonts = getFonts()
-      const str = transform(`${text}`, fonts[type])
+      const str = serif(`${text}`, { fontStyle: type })
 
       return interaction.editReply({ content:`${str}`, ephemeral:true})
     }
