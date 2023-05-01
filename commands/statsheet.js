e7775a875b7f5af1c803d7890182858989c613d1
Donovan Kong <kongdonovan@hotmail.com>
2023-04-30 17:30:04 -0700

    testing defense stats

---
+++ commands/statsheet.js
@@ -0,0 +1,65 @@
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+const { SlashCommandBuilder, SlashCommandUserOption, SlashCommandStringOption, SlashCommandIntegerOption, SlashCommandNumberOption, EmbedBuilder } = require('discord.js');
+const { getDBConnection } = require('../getDBConnection');
+const { admins } = require('../config.json');
+
+const roleOption = new SlashCommandStringOption()
+    .setRequired(true)
+    .setName('position')
+    .setDescription('The position to see the statsheet for.')
+    .addChoices(
+        {name:"Quarterback", value:"Quarterbacks"},
+        {name:"Wide Receiver", value:"Wide Receivers"},
+        {name:"Runningback", value:"Runningbacks"},
+        {name:"Defense", value:"Defenders"},
+        {name:"Kicker", value:"Kickers"},
+    )
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('getstats')
+        .setDescription('Gets the stats of all players in a particular position.')
+        .addStringOption(roleOption),
+    async execute(interaction) {
+        // needs to be implemented
+        const db = await getDBConnection();
+        const user = interaction.user.id
+        const guild = interaction.guild.id
+        const position = interaction.options.getString("position")
+
+        let str = ""
+        let stats
+
+        if (position === "Quarterbacks") {
+            stats = await db.all('SELECT * FROM QBStats WHERE guild = ? ORDER BY passer_rating', guild)
+
+            for (let i = 0; i < stats.length && i < 10; i++) {
+                const user = await interaction.guild.members.fetch(stats[i].discordid)
+
+                str += `**${i + 1})** ${user} \`${user.user.tag}\` - ${stats.passer_rating} passer rating, ${stats.yards} yards, ${stats.touchdowns} touchdowns, ${Math.round((stats.completions / stats.attempts) * 10) / 10}% completion percentage (${stats.completions} / ${stats.attempts})\n\n`
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${position} statsheet for ${interaction.guild.name}!`)
+                .setColor([0, 0, 0])
+                .setDescription(`${str}`)
+            
+            if (interaction.guild.iconURL()) {
+                embed.setThumbnail(interaction.guild.iconURL())
+            }
+    
+            if (interaction.user.avatarURL()) {
+                embed.setFooter({ text: `${interaction.user.tag}`, iconURL: `${interaction.user.avatarURL()}` })
+            } else {
+                embed.setFooter({ text: `${interaction.user.tag}` })
+            }
+            await db.close()
+            return interaction.editReply({ embeds:[embed], ephemeral:true })
+        }
+
+        await interaction.editReply("Not implemented yet!")
+        await db.close();
+        
+    }
+}
