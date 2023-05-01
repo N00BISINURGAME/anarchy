701dd8a6589b74ef7dca86a520448b2e723b04c4
Donovan Kong <kongdonovan@hotmail.com>
2023-04-30 19:24:09 -0700

    testing defense stats

--- commands/qbstats.js
+++ commands/qbstats.js
@@ -65,6 +65,8 @@ module.exports = {
         let passerRating = ((a + b + c + d) / 6) * 100
         passerRating = Math.round(passerRating * 10) / 10
 
+        console.log(passerRating)
+
         await db.run("UPDATE QBStats SET passer_rating = ? WHERE discordid = ? AND guild = ? AND season = ?", [passerRating, userid, guild, season])
         
         await db.close()
